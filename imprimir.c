a811937c5749d30df6ff1e5d20b3f3940726ddda
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2022-07-28 18:35:56 -0300

    Separando Funções em Arquivos

--- imprimir.c
+++ imprimir.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "pessoas.c"
 
 void imprimir(Pessoa p, char *nome_arquivo)
 {
--- registrar.c
+++ registrar.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "pessoas.c"
 
 void registrar(Pessoa p, char *nome_arquivo)
 {
